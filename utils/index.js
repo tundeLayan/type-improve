46d59253a7dee5ee9c5605b55d23164b89edb038
tundeLayan <tunderoxyafolayan@gmail.com>
2022-07-21 14:42:14 +0100

    utils file

---
+++ utils/index.js
@@ -0,0 +1,8 @@
+export const options = {
+    timeValues: [
+      {label:1, value: 1*60 },
+      {label:2, value: 2 * 60},
+      {label:5, value: 5 * 60},
+      {label:10, value: 10 * 60},
+    ]
+}
