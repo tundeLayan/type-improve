e9cf0955e5be2b1a9ece6c8eb914af163aab5ee7
tundeLayan <tunderoxyafolayan@gmail.com>
2022-07-21 14:44:51 +0100

    Input component, styling and unit test

---
+++ components/input/input.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from "./input.module.scss";
+
+// eslint-disable-next-line react/display-name
+const Input = React.forwardRef(({type="text",...rest}, ref) => (
+    <input data-testid="text-input" ref={ref} className={styles.input} {...{type}} {...rest} />
+  ));
+
+// Search.displayName = "Search";
+
+export default Input;
+
